13d3e69de6dcbf710112fb29c0bec51f9324ad87
Kenneth Soh <kenneth.soh@emq2.io>
2024-06-25 21:41:36 +0800

    Add imports

--- backend/src/main/java/com/example/backendapplication/configuration/SecurityConfiguration.java
+++ backend/src/main/java/com/example/backendapplication/configuration/SecurityConfiguration.java
@@ -16,9 +16,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 /**
  * Created on Ağustos, 2020
