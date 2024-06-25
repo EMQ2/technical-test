359ab48965251da1d8c59d2c210ff525e931718c
Kenneth Soh <kenneth@itachi1706.com>
2024-06-25 12:39:41 +0000

    Fix Gitpod

--- backend/src/main/java/com/example/backendapplication/BackendApplication.java
+++ backend/src/main/java/com/example/backendapplication/BackendApplication.java
@@ -1,5 +1,8 @@
 package com.example.backendapplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableAspectJAutoProxy
